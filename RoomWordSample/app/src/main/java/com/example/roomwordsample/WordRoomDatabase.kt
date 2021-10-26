84d409af844c7e67f5f018b100e19b80253f6ef3
rup-123 <93154469+rup-123@users.noreply.github.com>
2021-10-26 09:24:47 +0530

    Add files via upload
    1st commit

---
+++ RoomWordSample/app/src/main/java/com/example/roomwordsample/WordRoomDatabase.kt
@@ -0,0 +1,31 @@
+package com.example.roomwordsample
+
+import android.content.Context
+import androidx.room.Dao
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(entities = arrayOf(Word::class), version = 1, exportSchema = false)
+
+public abstract class WordRoomDatabase : RoomDatabase() {
+    abstract fun wordDao(): Dao
+
+    companion object {
+        @Volatile
+        private var INSTANCE: WordRoomDatabase? = null
+
+        fun getDatabase(context: Context): WordRoomDatabase {
+            return INSTANCE ?: synchronized(this) {
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    WordRoomDatabase::class.java,
+                    "word_database"
+                ).build()
+                INSTANCE = instance
+                // return instance
+                instance
+            }
+        }
+    }
+}
